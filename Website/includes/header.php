17d20b2d94895877c2c75447ea965f147f4e369f
Jeff Thompson <mail@jeffreythompson.org>
2018-12-11 18:03:38 -0500

    Tweaks for mobile

--- Website/includes/header.php
+++ Website/includes/header.php
@@ -3,7 +3,7 @@
 <head>
 
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" type="image/png" href="http://www.jeffreythompson.org/graphics/favicon.png">
 	<title>Collision Detection</title>
 
